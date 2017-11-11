d603f8dd89bd0008a4ce09368b800ece92aff7e2
Joe Johnson <joe@Joes-MacBook-Pro.local>
2017-11-11 12:00:56 -0800

    Began js logic.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-console.log("All's Ready, chumps!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardThree = "king";
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  alert("Match! Stars have aligned.");
+} else {
+  alert("Nope! Fate has you.");
+}
+
+
+
+
+// Version 1
+// console.log("All's Ready, chumps!");
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardThree = "king";
+// console.log("User flipped " + cardOne);
